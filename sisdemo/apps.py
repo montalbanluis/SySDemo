d0ee462735d9e8ad138bafe41ba39e0c7d590356
montalbanluis <lamvhaccc@gmail.com>
2021-08-13 01:14:10 -0400

    First Commit

---
+++ sisdemo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SisdemoConfig(AppConfig):
+    name = 'sisdemo'
---
+++ sisdemo/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from sisdemo.forms import RegisterForm
+
+from django.contrib.auth import authenticate, login, logout
+
+
+# Create your views here.
+
+
+
+def index(request):
+
+
+    return render(request, 'index.html')
+
+
+def panel(request):
+
+    return render(request, 'panel/panel.html',{'title': 'Home'})
+
+
+"""""
+def register(request):
+
+    return render(request, 'registro/registro.html',{'title': 'Registro'})
+
+"""
+
+
+
+def register(request):
+
+
+    if request.user.is_authenticated:
+        return redirect('panel')
+    else:
+
+        register_form = RegisterForm()
+
+        if request.method == 'POST':
+            register_form = RegisterForm(request.POST)
+
+            if register_form.is_valid():
+                register_form.save()
+
+                # CREAR MENSAJE FLASH
+                messages.success(request, 'Te haz registado bien ctm!')
+
+
+                return redirect('register')
+
+
+        return render(request, 'registro/registro.html',{
+            'title': 'Registro',
+            'form': register_form
+
+        })
+
+
+def login_user(request):
+
+     if request.user.is_authenticated:
+        return redirect('panel')
+     else:
+
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect('panel')
+
+            else:
+                messages.warning(request, 'No te haz identificado correctamente :(')
+
+
+
+        return render(request, 'index.html')
+
+
+
+def logout_user(request):
+    logout(request)
+
+    return redirect('index')
+
+
+
+
